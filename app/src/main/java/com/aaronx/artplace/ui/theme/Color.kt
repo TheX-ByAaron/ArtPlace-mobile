32fd928e9353d2b68f00b93340321efd007732d9
The X <haroune.official2@gmail.com>
2021-12-16 00:06:19 +0100

    Start: first commit

---
+++ app/src/main/java/com/aaronx/artplace/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.aaronx.artplace.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val RoyalBlack = Color(0xFF333333)
+val OceanBlue200 = Color(0xFFE1EFFB)
+val OceanBlue500 = Color(0xFF8BC2F2)
+val OceanBlue700 = Color(0xFF319AF6)
