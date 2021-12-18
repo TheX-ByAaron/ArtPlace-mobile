ae6fd35642b8a1ddebf65bc87ce9d1267b7a72b4
The X <haroune.official2@gmail.com>
2021-12-18 22:20:14 +0100

    Add: Icon Button

--- app/src/main/java/com/aaronx/artplace/ui/composables/Buttons.kt
+++ app/src/main/java/com/aaronx/artplace/ui/composables/Buttons.kt
@@ -1,5 +1,6 @@
 package com.aaronx.artplace.ui.composables
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
@@ -13,6 +14,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aaronx.artplace.R
@@ -71,16 +74,29 @@ fun AnimatedButton(modifier: Modifier = Modifier){
         , modifier = modifier
             .clip(MaterialTheme.shapes.small)
             .clickable {
-            isPlaying.value = !isPlaying.value
-            isLiked.value = !isLiked.value
-        })
+                isPlaying.value = !isPlaying.value
+                isLiked.value = !isLiked.value
+            })
+}
+
+@Composable
+fun IconButton(modifier: Modifier = Modifier){
+
+    Image(painter = painterResource(id = R.drawable.ic_messages)
+        , contentDescription = "icon button"
+        , colorFilter = ColorFilter.tint(MaterialTheme.colors.primary)
+        , modifier = modifier
+            .clip(MaterialTheme.shapes.small)
+            .clickable { }
+            .padding(all = 8.dp))
+
 }
 
 @Preview
 @Composable
 fun ButtonsPreview(){
     ArtPlaceTheme {
-        AnimatedButton(modifier = Modifier.size(50.dp))
+        IconButton(modifier = Modifier.size(45.dp))
     }
 
 }
