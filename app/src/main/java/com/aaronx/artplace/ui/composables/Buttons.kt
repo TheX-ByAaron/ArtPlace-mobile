b2a5f7608b8e21dcaaf8e0e0b0ec016e9292dd1f
The X <haroune.official2@gmail.com>
2021-12-18 21:54:13 +0100

    Add: Animated button using lottie

--- app/src/main/java/com/aaronx/artplace/ui/composables/Buttons.kt
+++ app/src/main/java/com/aaronx/artplace/ui/composables/Buttons.kt
@@ -1,20 +1,26 @@
 package com.aaronx.artplace.ui.composables
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.aaronx.artplace.R
 import com.aaronx.artplace.ui.theme.ArtPlaceTheme
 import com.aaronx.artplace.ui.theme.SurfaceColor
+import com.airbnb.lottie.compose.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -39,15 +45,42 @@ fun CButton(modifier:Modifier = Modifier
 
 }
 
-@Preview(showSystemUi = true)
+@Composable
+fun AnimatedButton(modifier: Modifier = Modifier){
+
+    //to Hoist later on
+    val isLiked = rememberSaveable { mutableStateOf(false)}
+    val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.like_animation))
+
+    //to keep as an internal state
+    val isPlaying = rememberSaveable{ mutableStateOf(false) }
+    val pressed by animateLottieCompositionAsState(
+        composition = composition,
+        isPlaying = isPlaying.value,
+        clipSpec = LottieClipSpec.Progress(0F, 0.5F),
+        iterations = 1,
+        restartOnPlay = true)
+    val notPressed by animateLottieCompositionAsState(
+        composition = composition,
+        isPlaying = true,
+        clipSpec = LottieClipSpec.Progress(0F, 0.1F))
+
+    LottieAnimation(
+        composition = composition
+        , progress = if(isLiked.value) pressed else notPressed
+        , modifier = modifier
+            .clip(MaterialTheme.shapes.small)
+            .clickable {
+            isPlaying.value = !isPlaying.value
+            isLiked.value = !isLiked.value
+        })
+}
+
+@Preview
 @Composable
 fun ButtonsPreview(){
     ArtPlaceTheme {
-        CButton(modifier= Modifier
-            .wrapContentWidth()
-            .wrapContentHeight()
-            .padding(start= 16.dp, end = 16.dp, top = 8.dp , bottom = 8.dp)
-            ,text = "button")
+        AnimatedButton(modifier = Modifier.size(50.dp))
     }
 
 }
