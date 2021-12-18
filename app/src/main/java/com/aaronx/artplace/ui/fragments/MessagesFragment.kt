7965ad48eb9475d209146281539a7ad91410c96a
The X <haroune.official2@gmail.com>
2021-12-18 22:50:13 +0100

    Finish: base messages fragment

--- app/src/main/java/com/aaronx/artplace/ui/fragments/MessagesFragment.kt
+++ app/src/main/java/com/aaronx/artplace/ui/fragments/MessagesFragment.kt
@@ -1,29 +1,30 @@
 package com.aaronx.artplace.ui.fragments
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.aaronx.artplace.ui.theme.ViewShape
-import com.aaronx.artplace.ui.theme.windowBackground
+import androidx.compose.ui.unit.dp
+import com.aaronx.artplace.ui.composables.MessageCard
 
 
 @Preview(showBackground = true, showSystemUi = false)
 @Composable
 fun MessagesFragment(){
     Box(modifier = Modifier
+        .padding(bottom = 74.dp, top = 8.dp)
         .fillMaxWidth()
-        .fillMaxHeight()
-        .background(color = MaterialTheme.colors.windowBackground, shape = MaterialTheme.shapes.ViewShape)
-        , contentAlignment = Alignment.Center){
+        .fillMaxHeight()){
 
-        Text("Messages fragment")
+        LazyColumn{
+            items(20){
+                MessageCard()
+            }
+        }
     }
 
 }
