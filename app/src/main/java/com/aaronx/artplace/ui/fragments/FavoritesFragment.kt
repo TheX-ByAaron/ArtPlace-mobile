ec82c07f1aefed5771ec17c1a090fe61c69747eb
The X <haroune.official2@gmail.com>
2021-12-18 22:28:04 +0100

    Add: favorites base fragment

--- app/src/main/java/com/aaronx/artplace/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/aaronx/artplace/ui/fragments/FavoritesFragment.kt
@@ -1,28 +1,34 @@
 package com.aaronx.artplace.ui.fragments
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.aaronx.artplace.ui.theme.ViewShape
-import com.aaronx.artplace.ui.theme.windowBackground
+import androidx.compose.ui.unit.dp
+import com.aaronx.artplace.ui.composables.PostCard
 
 
+@OptIn(ExperimentalFoundationApi::class)
 @Preview(showBackground = true, showSystemUi = false)
 @Composable
 fun FavoritesFragment(){
-    Box(modifier = Modifier
+    Box(modifier = Modifier.padding(bottom = 74.dp, top = 8.dp)
         .fillMaxWidth()
-        .fillMaxHeight()
-        .background(color = MaterialTheme.colors.windowBackground, shape = MaterialTheme.shapes.ViewShape)
-        , contentAlignment = Alignment.Center){
-        Text("Settings fragment")
+        .fillMaxHeight()){
+
+        LazyVerticalGrid(cells = GridCells.Adaptive(minSize = 110.dp)
+            , modifier = Modifier.padding(start = 8.dp, end = 8.dp)){
+            items(20){
+                PostCard()
+            }
+        }
+
     }
 
 }
